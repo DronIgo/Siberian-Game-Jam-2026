43f577625acf727fbe84871ae7678ccc503fc05a
Daniil Beloglazov <zheltogsan@gmail.com>
2026-05-03 15:13:21 +0700

    start dialog

--- Hub/CityStateHolder.gd
+++ Hub/CityStateHolder.gd
@@ -2,5 +2,5 @@ extends Node
 
 var buildings:  Dictionary = {
 	"shop": BuildingInfo.new("shop", "This is The Shop", "shop"),
-	"house": BuildingInfo.new("house", "This is The House", "house_dialog")
+	"house": BuildingInfo.new("house", "This is The House", "house_fight")
 }
