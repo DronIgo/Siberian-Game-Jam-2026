832ed5a77a5e0c8d7dd6a530255d5afa606aae4b
sintaxiz <natashaleind@gmail.com>
2026-05-03 18:12:46 +0700

    skip text by mouse

--- Scripts/UI/Fight/ActionDisplayText.gd
+++ Scripts/UI/Fight/ActionDisplayText.gd
@@ -4,6 +4,8 @@ extends Control
 @export var label : Label
 @export var wait_after_display : float = 1.0
 
+var _display_timer: SceneTreeTimer
+
 func _ready() -> void:
 	clear()
 
@@ -14,10 +16,20 @@ func clear() -> void:
 func display_action(result: ActionResult) -> void:
 	await display(result.get_formatted_description())
 
-func display(text : String, time: float = wait_after_display) -> void:
-	#TODO: LOW PRIORITY постепенный вывод
+func display(text: String, time: float = wait_after_display) -> void:
 	visible = true
 	label.text = text
-	#TODO: ожидание зависит от длины текста
-	await get_tree().create_timer(time).timeout
+	_display_timer = get_tree().create_timer(time)
+	await _display_timer.timeout
 	visible = false
+	_display_timer = null
+
+
+func _on_display_done() -> void:
+	visible = false
+	_display_timer = null
+
+func _input(event: InputEvent) -> void:
+	if visible and event is InputEventMouseButton and event.pressed:
+		if _display_timer:
+			_display_timer.timeout.emit()
