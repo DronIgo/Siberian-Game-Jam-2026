d9214de69eb9ea56c39e553e1a89af9b9c8acf47
sintaxiz <natashaleind@gmail.com>
2026-05-03 21:25:24 +0700

    summon all organs

--- Scripts/Logic/Fight/Organs/OrganSummoner.gd
+++ Scripts/Logic/Fight/Organs/OrganSummoner.gd
@@ -3,11 +3,28 @@ extends Node2D
 
 # Не использую прелоад, чтобы не загружать 20 сцен на старте каждой сцены
 #TODO: для маленького тентакля всетаки сделать preload - он может появляться много раз за бой
+const HEALTHY_BRAIN_UID = "uid://uf3j8pfvapgu"
 const HEALTHY_HEART_UID = "uid://x6staq5pspoj"
+const HEALTHY_KIDNEY_UID = "uid://btnrekgkv732r"
+const HEALTHY_LIVER_UID = "uid://nk24rqijc65m"
+const HEALTHY_LUNG_UID = "uid://xyi2308s1taw"
 
-const SICK_KIDNEY_UID = "uid://taivcdbkqdb8"
+const SICK_BRAIN_UID = "uid://clabf2ncd26ot"
+const SICK_HEART_UID = "uid://bsnboj8pl0l1s"
+const SICK_KIDNEY_UID = "uid://x5e5setqg4l1"
+const SICK_LIVER_UID = "uid://taivcdbkqdb8"
+const SICK_LUNG_UID = "uid://cfpbydsb11qf3"
 
+const EVIL_ACSOLOTL_UID = "uid://d1pt6ctp3mqfq"
+const EVIL_EYES_UID = "uid://r04p8yth0du5"
+const EVIL_FIREFLY_UID = "uid://bljexjlp8plf8"
+const EVIL_HORNS_UID = "uid://cqromdfbponiy"
+const EVIL_SCALES_UID = "uid://dshk8vjd08w0m"
+const EVIL_SHELL_UID = "uid://cax6rrsn0t7cn"
+const EVIL_SMALL_TENTACLE_UID = "uid://b4bx5348yp1sy"
+const EVIL_TAIL_UID = "uid://dh5ak6lpn3jxv"
 const EVIL_TENTACLE_UID = "uid://doa8o4i0u2wco"
+const EVIL_WINGS_UID = "uid://cnwl7bwcsuhqj"
 
 @export var slots_container: Node2D
 @export var friendly_slots : Array[Node2D]
@@ -23,12 +40,46 @@ var _shifted_down : bool = false
 func summon_by_name(organ_name : String) -> OrganBase:
 	var organ_res
 	match organ_name:
+		"healthy_brain":
+			organ_res = load(HEALTHY_BRAIN_UID)
 		"healthy_heart":
 			organ_res = load(HEALTHY_HEART_UID)
+		"healthy_kidney":
+			organ_res = load(HEALTHY_KIDNEY_UID)
+		"healthy_liver":
+			organ_res = load(HEALTHY_LIVER_UID)
+		"healthy_lung":
+			organ_res = load(HEALTHY_LUNG_UID)
+		"sick_brain":
+			organ_res = load(SICK_BRAIN_UID)
+		"sick_heart":
+			organ_res = load(SICK_HEART_UID)
 		"sick_kidney":
 			organ_res = load(SICK_KIDNEY_UID)
+		"sick_liver":
+			organ_res = load(SICK_LIVER_UID)
+		"sick_lung":
+			organ_res = load(SICK_LUNG_UID)
+		"acsolotl":
+			organ_res = load(EVIL_ACSOLOTL_UID)
+		"eyes":
+			organ_res = load(EVIL_EYES_UID)
+		"firefly":
+			organ_res = load(EVIL_FIREFLY_UID)
+		"horns":
+			organ_res = load(EVIL_HORNS_UID)
+		"scales":
+			organ_res = load(EVIL_SCALES_UID)
+		"shell":
+			organ_res = load(EVIL_SHELL_UID)
+		"small_tentacle":
+			organ_res = load(EVIL_SMALL_TENTACLE_UID)
+		"tail":
+			organ_res = load(EVIL_TAIL_UID)
 		"tentacle":
 			organ_res = load(EVIL_TENTACLE_UID)
+		"wings":
+			organ_res = load(EVIL_WINGS_UID)
 	var organ = organ_res.instantiate()
 	if organ_name == "tentacle":
 		organ.actor.organ_summoner = self
