2fe501f45a6d7034c29cc08054bce0b42ba0afee
Daniil Beloglazov <zheltogsan@gmail.com>
2026-05-03 22:14:02 +0700

    days texts

--- Dialog/ReplicasBox.gd
+++ Dialog/ReplicasBox.gd
@@ -6,7 +6,7 @@ extends Control
 @export var template: String = "{speaker_name}:\n{text}"
 
 func new_replica(data: ReplicaData):
-	if data.speaker_name == null:
+	if data.speaker_name == null or data.speaker_name == "":
 		text_label.text = data.text
 		return
 	text_label.text = template.format({ "speaker_name": data.speaker_name, "text": data.text })
